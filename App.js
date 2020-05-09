59c388d52bc0d5ffee2e1d9e720d0b83a7eea239
ivan <oscari.lopez@potros.itson.edu.mx>
2020-05-09 16:50:04 -0700

    commit

--- App.js
+++ App.js
@@ -1,19 +1,34 @@
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React from "react";
+import { StyleSheet } from "react-native";
+import { NavigationContainer } from "@react-navigation/native";
+import { createStackNavigator } from '@react-navigation/stack';
 
-export default function App() {
+import Login from "./screens/auth/Login";
+import newAccount from "./screens/auth/newAccount";
+
+const Stack = createStackNavigator();
+
+const App = () => {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-    </View>
+    <NavigationContainer>
+      <Stack.Navigator>
+        <Stack.Screen 
+        name="Login" 
+        component={Login}
+        options={{ headerShown: false }}
+        />
+        <Stack.Screen 
+        name="newAccount" 
+        component={newAccount}
+        options={{ headerShown: false }}
+        />
+      </Stack.Navigator>
+    </NavigationContainer>
   );
-}
+};
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
+const style = StyleSheet.create({
+
 });
+
+export default App;
---
+++ screens/auth/newAccount.js
@@ -0,0 +1,149 @@
+import React, { useState } from 'react';
+import { View, Text, StyleSheet, TextInput, Image, TouchableOpacity } from 'react-native'
+
+
+const NewAccount = () => {
+
+    //! State del usuario
+    const [user, setUser] = useState({
+        name: '',
+        email: '',
+        password: ''
+    });
+
+    // //! destructuracion del state
+    const { name, email, password } = user;
+
+    const onChange = (e) => {
+        guardarUsuario({
+            ...usuario,
+            [e.target.name]: e.target.value
+        })
+    }
+
+    const onSubmit = e => {
+        e.preventDefault();
+
+        //validar que no haya campos vacios
+        if (email.trim() === '' || password.trim() === '') {
+            mostrarAlerta('Todos los campos son obligatorios', 'alerta-error');
+        }
+        //pasarlo al action
+        // iniciarSesion({ email, password });
+    }
+    return (
+        // Container
+        <View style={styles.container}>
+            <Image style={styles.image} source={require('../../assets/ITSON_azul.png')}></Image>
+            {/* Formulario container  */}
+            <View style={styles.formContainer}>
+
+                {/* header */}
+                <Text style={styles.header}>Inicia Sesion</Text>
+
+                {/* formulario */}
+                <Text style={[styles.text, styles.textCenter]}>Porvafor llena todos los espacios</Text>
+
+                <Text style={[styles.text, styles.textInput, styles.mt]}>Nombre:</Text>
+                <TextInput
+                    style={styles.formInput}
+                    value={name}
+                    onChangeText={onSubmit}
+                    placeholder={"  Nombre Aqui"}
+                >
+                </TextInput>
+
+                <Text style={[styles.text, styles.textInput]}>Correo:</Text>
+                <TextInput
+                    style={styles.formInput}
+                    value={email}
+                    onChangeText={onSubmit}
+                    placeholder={"  Correo Aqui"}
+                >
+                </TextInput>
+
+                <Text style={[styles.text, styles.textInput]}>Password:</Text>
+                <TextInput
+                    style={styles.formInput}
+                    value={password}
+                    onChangeText={onSubmit}
+                    placeholder={"  Password Aqui"}
+                >
+                </TextInput>
+                <View style={styles.newAccount}>
+                    <Text style={styles.textNewAcount}>¿No tienes una cuenta?</Text>
+                    <TouchableOpacity
+                        onPress={() => {
+                            navigation.navigate('newAccount')
+                        }}
+                    >
+                        <Text style={[styles.text, styles.textInput]}>Crea una</Text>
+                    </TouchableOpacity>
+                </View>
+            </View>
+        </View>
+    );
+}
+
+const styles = StyleSheet.create({
+    formContainer: {
+
+        justifyContent: 'center',
+        backgroundColor: '#0669B0',
+        width: 380,
+        borderRadius: 20
+    },
+    container: {
+        alignItems: 'center',
+        justifyContent: 'center',
+        backgroundColor: '#fff',
+    },
+    formInput: {
+        width: 350,
+        height: 50,
+        borderRadius: 10,
+        marginVertical: 20,
+        marginLeft: 15,
+        backgroundColor: '#fff'
+
+    },
+    header: {
+        color: 'black',
+        fontSize: 25,
+        fontWeight: '700',
+        textAlign: 'center',
+        marginVertical: 10,
+        color: '#fff'
+    },
+    text: {
+        color: '#fff',
+        fontWeight: '700',
+
+    },
+    textInput: {
+        marginLeft: 10
+    },
+    textCenter: {
+        textAlign: 'center'
+    },
+    mt: {
+        marginTop: 10
+    },
+    image: {
+        marginTop: 100,
+        width: 350,
+        height: 150,
+        resizeMode: 'contain'
+    },
+    newAccount: {
+        flexDirection: 'row',
+        marginBottom: 30
+    },
+    textNewAcount: {
+        color: '#fff',
+        marginLeft: 10
+    }
+
+});
+
+export default NewAccount;
